4dc495887f099a7fa9096860adb68eefebe3c5f2
Jeetpal <singhjeetpal99@gmail.com>
2022-08-07 21:37:27 -0500

    Removed a bug causing navbar to repeat itself

--- components/Nav.js
+++ components/Nav.js
@@ -1,16 +1,21 @@
 import requests from "../utils/requests";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 function Nav() {
   const router = useRouter();
+  const [currentNav, setCurrentNav] = useState("");
   return (
     <nav className="relative">
       <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
         {Object.entries(requests).map(([key, { title, url }]) => (
           <h2
             key={key}
-            onClick={() => router.push(`/?genre=${key}`)}
-            className="last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-whtie active:text-red-500"
+            onClick={() => {
+              router.push(`/?genre=${key}`);
+              setCurrentNav(title);
+            }}
+            className={`last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-whtie active:text-red-500 ${currentNav === title?"scale-125 text-red-500":""}`}
           >
             {title}
           </h2>
